93365fdbb768ff2ded0b9616d89638e96fc45a3b
ahmedjidar <justup1080@gmail.com>
2023-08-30 00:56:27 +0200

    TMT CL v1.0.5 Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {} from 'react-bootstrap';
 import { AppLayout } from "./Components/export";
@@ -7,9 +7,19 @@ import "./App.css";
 
 const App = () => {
 
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/hello')  // Adjust the URL as needed
+    .then(response => response.text())
+    .then(data => setResponse(data))
+    .catch(error => console.error(error));
+  }, [])
+
   return (
     <Router>
       <AppLayout/>
+      <h2>This is a message from the Server: {response}</h2>
     </Router>
   );
 }
